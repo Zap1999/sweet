b57396ee6207272805f341ffa25856ae381a6ba3
avash <windowsuser1223334444@gmail.com>
2020-05-11 21:57:32 +0300

    trigger updated

--- Scripts/UpdateManufacturingOrderStatusTrigger.sql
+++ Scripts/UpdateManufacturingOrderStatusTrigger.sql
@@ -2,25 +2,74 @@
     ON [dbo].[manufacturing_order]
     FOR UPDATE
     AS
-    
+
 BEGIN
+    DECLARE @SweetStorage TABLE
+                          (
+                              SweetId   BIGINT,
+                              FactoryId BIGINT,
+                              Count     BIGINT,
+                              Processed INT
+                          );
 
-    UPDATE sweet_storage
-
-    SET sweet_storage.count = sweet_storage.count + moi.count
-
-    FROM inserted
-             JOIN manufacturing_order mo ON mo.id = inserted.ID
+    INSERT INTO @SweetStorage
+    SELECT moi.sweet_id,
+           f.id,
+           moi.count,
+           0
+    FROM inserted,
+         manufacturing_order mo
              JOIN factory_unit fu on mo.factory_unit_id = fu.id
              JOIN manufacturing_order_item moi on mo.id = moi.manufacturing_order_id
              JOIN factory f on fu.factory_id = f.id
-
-    WHERE sweet_storage.sweet_id = moi.sweet_id
-      AND sweet_storage.factory_id = f.id
+    WHERE mo.id = inserted.id
       AND mo.status_id = (
         SELECT id
         FROM manufacturing_order_status
         WHERE name = N'Виконано'
-    )
+    );
 
+    DECLARE @SweetId BIGINT;
+    DECLARE @FactoryId BIGINT;
+    DECLARE @Count BIGINT;
+
+    WHILE (
+              SELECT COUNT(*)
+              FROM @SweetStorage
+              WHERE Processed = 0
+          ) > 0
+        BEGIN
+            SELECT TOP 1 @SweetId = SweetId,
+                         @FactoryId = FactoryId,
+                         @Count = Count
+            FROM @SweetStorage
+            WHERE Processed = 0
+
+
+            IF EXISTS(
+                    SELECT *
+                    FROM sweet_storage ss
+                    WHERE ss.sweet_id = @SweetId
+                      AND ss.factory_id = @FactoryId
+                )
+                BEGIN
+                    UPDATE sweet_storage
+                    SET sweet_storage.count = sweet_storage.count + @Count
+                    WHERE sweet_storage.sweet_id = @SweetId
+                      AND sweet_storage.factory_id = @FactoryId;
+                END
+            ELSE
+                BEGIN
+                    INSERT INTO sweet_storage (sweet_id, factory_id, [count])
+                    VALUES (@SweetId, @FactoryId, @Count);
+                END
+
+
+            UPDATE @SweetStorage
+            SET Processed = 1
+            WHERE SweetId = @SweetId
+              AND FactoryId = @FactoryId
+              AND Count = @Count
+
+        END
 END
